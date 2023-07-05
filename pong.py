017bed20f3e13505c86bf989b06186f8f40da1e3
Walid Abdullah <walid@hufs.ac.kr>
2023-07-05 16:14:49 +0900

    bind

--- pong.py
+++ pong.py
@@ -50,6 +50,7 @@ class Simulate:
 
     def bind_keys(self):
         self.window.listen()
+        self.window.onkey(self.pong.playerL_up, "w")
         # MEMBER-1
         # add four key-events: w,s keys: playerL_up:down, Up,Down arrows: PlayerR_up:down
         # e.g. self.window.onkey(<func_to_run>, <key:string>)
